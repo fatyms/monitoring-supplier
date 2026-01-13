780658d683141558ee2cc750c057f268460dfbcb
fatyms <div.fatyms01@gmail.com>
2026-01-13 09:35:24 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 // Supabase config
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = 'https://onagcdgzuqoqtowwfvmu.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uYWdjZGd6dXFvcXRvd3dmdm11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5MzU1NDAsImV4cCI6MjA4MTUxMTU0MH0.YydT3OZ6_S1ewnrG4HamOg2eoNE63eYvPolvFtjMNhs';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // ===================== Supplier =====================
