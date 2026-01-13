47bf65569728f3da6d8a7a3845e0f438edbcdf33
fatyms <div.fatyms01@gmail.com>
2026-01-13 09:59:47 +0700

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+<script type="module">
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+export const supabase = createClient(
+  'https://onagcdgzuqoqtowwfvmu.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uYWdjZGd6dXFvcXRvd3dmdm11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5MzU1NDAsImV4cCI6MjA4MTUxMTU0MH0.YydT3OZ6_S1ewnrG4HamOg2eoNE63eYvPolvFtjMNhs'
+)
+</script>
